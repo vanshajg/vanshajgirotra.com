5f8c4a2109bcf551f7d272dd620c7f5176c6c748
Vanshaj Girotra <vanshaj.g@practo.com>
2020-12-12 00:46:12 +0530

    Move window to useeffect

--- src/components/home/intro/contacts.jsx
+++ src/components/home/intro/contacts.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import data from './contacts_data'
 import { OutboundLink } from 'gatsby-plugin-google-gtag'
 
 const getContact = ({ icon: Icon, name, url }, index) => {
   return (
     <OutboundLink href={url} rel="noopener noreferrer" className="mr-4 text-3xl animate-fade_in" key={index}
+      // check why opacity: 0 had to be given
       style={{ animationDelay: `${index * 0.25 + 0.25}s`, animationDuration: '1s', opacity: 0 }}>
       <Icon />
     </OutboundLink>
@@ -13,9 +14,12 @@ const getContact = ({ icon: Icon, name, url }, index) => {
 
 const Contacts = () => {
   const [is_loaded, setLoaded] = useState(false)
-  window.addEventListener('load', () => {
-    setLoaded(true)
-  })
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      setLoaded(true)
+    })
+  }, [])
+
 
   if (!is_loaded)
     return null
