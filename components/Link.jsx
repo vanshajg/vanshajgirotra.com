2e627d442e30c27a3285d24c1713ffb70bace47b
vanshaj <vgirotra@godaddy.com>
2024-05-08 12:20:04 +0530

    fixees

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,19 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import Link from 'next/link';
-import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 
-const CustomLink = ({
-  href,
-  ...rest
-}: DetailedHTMLProps<
-  AnchorHTMLAttributes<HTMLAnchorElement>,
-  HTMLAnchorElement
->) => {
+const CustomLink = ({ href, ...rest }) => {
   const isInternalLink = href && href.startsWith('/');
   const isAnchorLink = href && href.startsWith('#');
 
   if (isInternalLink) {
-    return <Link href={href} {...rest}></Link>;
+    return <Link href={href} {...rest} />;
   }
 
   if (isAnchorLink) {
